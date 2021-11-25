024edfb6befb1e34c1b419af87da8a4560c63368
weskrr <wielandfriedrichwuller@gmail.com>
2021-11-25 09:01:03 +0100

    Renamed files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Example from './Example';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Example />
   </React.StrictMode>,
   document.getElementById('root')
 );
